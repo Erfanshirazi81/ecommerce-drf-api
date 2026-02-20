cb7e5f3659ac437ed030a309ec471d0b78d670ff
ErfanShirazi <s.shirazi2012.es@gmail.com>
2026-02-20 13:23:03 +0330

    add accounts app and register user

---
+++ accounts/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response    
+from django.contrib.auth.models import User 
+from .serializers import UserRegisterSerializer
+
+
+
+class UserRegisterView(APIView):
+    serializer_class = UserRegisterSerializer
+    
+    def post(self, request):
+        ser_data = UserRegisterSerializer(data = request.data)
+        if ser_data.is_valid():
+            ser_data.create(ser_data.validated_data)
+            return Response({"message": "User registered successfully!"})
+        return Response(ser_data.errors)
